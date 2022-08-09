abec22570bf2672f8158c7d4fd66a517a3186328
Igor_U <fixtor75@gmail.com>
2022-08-10 02:31:59 +0300

    upd..

--- Exe2/Program.cs
+++ Exe2/Program.cs
@@ -4,10 +4,10 @@ if (number < 100)
     Console.WriteLine($"Вы ввели число меньше трехзначного {number} и нет третьей цифры");
 else
 {
-    while (number > 100)
+    while (number >= 10)
     {
-        number = (number % 10) / 100;
-        Console.WriteLine($"число во втором регистре: {number}");
+        number = (number % 100) / 10;
     }
+    Console.WriteLine($"число во втором регистре: {number}");
 }
 
