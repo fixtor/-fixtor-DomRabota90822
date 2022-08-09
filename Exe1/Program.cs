edb5e440ba1986768412cd287fdda5aebe9f1509
Igor_U <fixtor75@gmail.com>
2022-08-10 00:34:51 +0300

    Задание 10 готово

--- Exe1/Program.cs
+++ Exe1/Program.cs
@@ -1,3 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-// Подготовка к семинару
+﻿Console.WriteLine("Введите трехзначное число");
+int numberThreeRegister = Convert.ToInt32(Console.ReadLine());
+if ((numberThreeRegister > 99) && (numberThreeRegister < 1000))
+{
+    int secondNumberOf = Convert.ToInt32((numberThreeRegister % 100) / 10);
+    Console.WriteLine($"Вы ввели трехзначное число: {numberThreeRegister}, число во втором регистре: {secondNumberOf}");
+}
+else
+    Console.WriteLine($"Вы ввели неверное число {numberThreeRegister}");
