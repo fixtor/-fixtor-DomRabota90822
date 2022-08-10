b3688ec5c5a131ae9072d7e913ec2cf15b17e0f3
Igor_U <fixtor75@gmail.com>
2022-08-10 13:20:32 +0300

    Вариант задания 13 с положительными и отрицательными числами

---
+++ Exe2.Full/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите любое число, не меньше 100, и не больше -100");
+int number = Convert.ToInt32(Console.ReadLine());
+if ((number < 100) && (number > -100))
+    Console.WriteLine($"Вы ввели число меньше трехзначного {number} и нет третьей цифры");
+else
+{
+    while (number > 999 || number < -999)
+    {
+        number = number / 10;
+    }
+    Console.WriteLine($"число во третьем регистре: {number % 10}");
+}
