9b4fdf58ddaa2f30edc435915473cdc49750f2d6
Igor_U <fixtor75@gmail.com>
2022-08-10 19:48:21 +0300

    Задание дополнительное но1 готово

---
+++ ExtraExe1/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+Console.WriteLine("Введите диапазон чисел от n1");
+int n1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(" до n2");
+int n2 = Convert.ToInt32(Console.ReadLine());
+int temp = 0;
+if (n1 < n2)
+{
+    for (; n1 <= n2; n1++)
+    {
+        if (n1 % 2 != 0)
+            temp = n1 + temp;
+    }
+    Console.WriteLine($"Сейчас: {temp}");
+}
+else
+    Console.WriteLine("Вы ввели неверный диапазон");
