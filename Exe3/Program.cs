e01b26c3e6e2823c4edc0da84d7d8a02633941f6
Igor_U <fixtor75@gmail.com>
2022-08-10 12:51:37 +0300

    Задача 15 готова

---
+++ Exe3/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Clear();
+int Saturday = 6;
+int Sunday = 7;
+Console.WriteLine($"Введите номер дня недели: ");
+int dayWeek = Convert.ToInt32(Console.ReadLine());
+if (dayWeek == Saturday || dayWeek == Sunday)
+    Console.WriteLine("Выходной!");
+else Console.WriteLine("Будни...");
